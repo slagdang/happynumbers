8405274796ebceb0f21ae5fe75fb158be60293dd
Steve Bollinger <mail4steveb@yahoo.com>
2013-04-26 20:44:59 -0700

    Added two modes to happynum5, either store enough previous results to include at least one link in every loop or enough to include every link in every loop. The latter is faster but uses more memory. Fixed smallestgrowerfinder to determine the sizes of the two sets of previous results.

--- happynum5.cpp
+++ happynum5.cpp
@@ -1,14 +1,18 @@
-
-#include <map>
-
 #include "happynum.h"
+
+// Every non-terminal loop must have at least one step in it which produces an
+// transformed values greater than itself. There are no numbers above 162 which produce
+// numbers greater than the input so every loop must have at least one number below 163.
+// In fact every loop must have at least one number below 100. But every value in a loop
+// must be less than 163, so we store the results of all values below 163.
+#define ENTIRELOOPSET 1
+
+// Do we allow negative numbers as input? Not needed for test, but maybe for completeness.
+#define ALLOWNEGINPUT 0
 
 static int happysummation(int num)
 {
     int         sum = 0;
-
-//    if (num < 0)
-//        num = -num;
 
     while (num != 0)
     {
@@ -21,6 +25,8 @@ static int happysummation(int num)
     return sum;
 }
 
+#if defined(ENTIRELOOPSET) && ENTIRELOOPSET
+// Store every value that can be in a loop.
 const int loopitems = 163;
 
 char happyres[loopitems] =
@@ -37,6 +43,23 @@ char happyres[loopitems] =
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
     -1, -1, -1
 };
+#else
+const int loopitems = 100;
+
+char happyres[loopitems] =
+{
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+};
+#endif
 
 void InitHappy(void)
 {
@@ -59,6 +82,7 @@ static bool IsHappyLooper(int innum)
 // then update the number with what we get by checking on it
     bool         rethappy = IsHappyLooper(happied);
 
+// store result so we don't calculate it again.
     happyres[innum] = rethappy;
 
     return rethappy;
@@ -68,7 +92,6 @@ static bool IsHappyBigUn(int innum)
 {
     int     happied = happysummation(innum);
 
-// mark this number as non-happy in case we loop back to it
     bool    rethappy = happied < loopitems ? IsHappyLooper(happied) : IsHappyBigUn(happied);
 
     return rethappy;
@@ -76,5 +99,10 @@ static bool IsHappyBigUn(int innum)
 
 bool IsHappy(int innum)
 {
+#if defined(ALLOWNEGINPUT) && ALLOWNEGINPUT
+    if (innum < 0)
+        innum = -innum;
+#endif
+
     return innum < loopitems ? IsHappyLooper(innum) : IsHappyBigUn(innum);
 }
--- smallestgrowerfinder.cpp
+++ smallestgrowerfinder.cpp
@@ -26,18 +26,39 @@ static int happysummation(int num)
     return sum;
 }
 
+const int checknums = 163;
+
 int main(int argc, const char *argv[])
 {
-	int			checkit;
+	int				checkit;
+	int				sum[checknums];
+	int				biggestsofar = 0;
+	int				xformbound = 0;
+	int				xform;
 
-	for (checkit = 162; checkit > 1; checkit--)
+	for (checkit = (checknums - 1); checkit >= 0; checkit--)
 	{
-		int			xform = happysummation(checkit);
+		int	xform = happysummation(checkit);
 
-		if (xform > checkit)
+		sum[checkit] = xform;
+
+		if (0 == xformbound && xform > checkit)
 		{
 			std::cout << "The smallest number which produces a value larger than itself is " << checkit
 			          << " and it produces the value " << xform << std::endl;
+			xformbound = xform;
+		}
+	}
+
+// now find the biggest number that is transformed from 0 the found smallest
+	for (checkit = 2; checkit <= checknums; checkit++)
+	{
+		if (sum[checkit] > biggestsofar)
+			biggestsofar = sum[checkit];
+
+		if (biggestsofar <= checkit)
+		{
+			std::cout << "Every transformed number below " << checkit << " is less than or equal to " << checkit << std::endl;
 			break;
 		}
 	}
